1c9db164b6ecbb069c7b35464fb730cddba58808
Owen Conti <owen@ohseemedia.com>
2016-03-11 17:33:35 -0700

    Fixed tests 3 & 4, added test for keeping value selected when multiple=false and options change

--- test/unit/Select.spec.js
+++ test/unit/Select.spec.js
@@ -32,23 +32,72 @@ describe('Select.vue', () => {
     expect(vm.$children[0].value).toEqual(['one'])
   })
 
-  /**
-   * TODO: Right now this only works for arrays of strings.. But the same method
-   * should apply to arrays of objects.
-   */
-  // it('can accept an array of objects and pre-selected values', () => {
-  //   const vm = new Vue({
-  //     template: '<div><v-select :value.sync="value"></v-select></div>',
-  //     components: { vSelect },
-  //     data: {
-  //       value: [{label: 'This is Foo', value: 'foo'}],
-  //       options: [{label: 'This is Foo', value: 'foo'}, {label: 'This is Bar', value: 'bar'}]
-  //     }
-  //   }).$mount()
-  //
-  //   expect(vm.$children[0].$get('value')).toEqual({label: 'This is Foo', value: 'foo'})
-  // })
-  //
+  it('can accept an array of objects and pre-selected value (single)', () => {
+    const vm = new Vue({
+      template: '<div><v-select :value.sync="value"></v-select></div>',
+      components: { vSelect },
+      data: {
+        value: {label: 'This is Foo', value: 'foo'},
+        options: [{label: 'This is Foo', value: 'foo'}, {label: 'This is Bar', value: 'bar'}]
+      }
+    }).$mount()
+
+    expect( vm.$children[0].$get('value').value ).toEqual( 'foo' )
+    expect( vm.$children[0].$get('value').label ).toEqual( 'This is Foo' )
+  })
+
+  it('can accept an array of objects and pre-selected values (multiple)', () => {
+    const vm = new Vue({
+      template: '<div><v-select :value.sync="value" :multiple="true"></v-select></div>',
+      components: { vSelect },
+      data: {
+        value: [{label: 'This is Foo', value: 'foo'}, {label: 'This is Bar', value: 'bar'}],
+        options: [{label: 'This is Foo', value: 'foo'}, {label: 'This is Bar', value: 'bar'}]
+      }
+    }).$mount()
+
+    var values = vm.$children[0].$get('value')
+    var labels = []
+
+    labels = values.map( value => value.label )
+    values = values.map( value => value.value )
+
+    expect( values ).toEqual( ['foo', 'bar'] )
+    expect( labels ).toEqual( ['This is Foo', 'This is Bar'] )
+  })
+
+  it('removes the last item in the value array on delete keypress when multiple is true', () => {
+
+    const vm = new Vue({
+      template: '<div><v-select :value.sync="value" :multiple="true"></v-select></div>',
+      components: { vSelect },
+      data: {
+        value: ['one','two'],
+        options: ['one','two','three']
+      }
+    }).$mount()
+
+    vm.$children[0].maybeDeleteValue()
+    Vue.nextTick(() => {
+      expect(vm.$children[0].$get('value')).toEqual(['one'])
+    })
+  })
+
+  it('sets the value to null on delete keypress when multiple is false', () => {
+    const vm = new Vue({
+      template: '<div><v-select :value.sync="value"></v-select></div>',
+      components: { vSelect },
+      data: {
+        value: 'one',
+        options: ['one','two','three']
+      }
+    }).$mount()
+
+    vm.$children[0].maybeDeleteValue()
+    Vue.nextTick(() => {
+      expect(vm.$children[0].$get('value')).toEqual(null)
+    })
+  })
 
   it('can determine if the value prop is empty', () => {
     const vm = new Vue({
@@ -83,8 +132,8 @@ describe('Select.vue', () => {
       }
     }).$mount()
 
-    vm.$children[0].$set( 'options', ['four','five','six'] )
-    Vue.nextTick(function() {
+    vm.$children[0].options = ['four','five','six']
+    Vue.nextTick(() => {
       expect(vm.$children[0].$get('value')).toEqual([])
       done()
     })
