434251b0ae66378560f1741d4d37762efad36856
Ali Almoullim <ali.almoullim@gmail.com>
2018-10-23 19:41:00 +0300

    Fix "dropdown items" not being retured when filterable is false and taggable is true

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1112,7 +1112,7 @@
        * @return {array}
        */
       filteredOptions() {
-        if (!this.filterable && !this.taggable) {
+        if (!this.filterable) {
           return this.mutableOptions.slice()
         }
         let options = this.search.length ? this.filter(this.mutableOptions, this.search, this) : this.mutableOptions;
