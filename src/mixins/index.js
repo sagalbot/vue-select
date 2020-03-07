98d52c97616b8cdec7d0dd625c79cd07d258a6a7
Oleksii Teterin <altmoc@gmail.com>
2020-03-07 18:13:12 +0100

    Remove domHelpers from mixins

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,6 +1,5 @@
 import ajax from './ajax'
 import pointer from './typeAheadPointer'
 import pointerScroll from './pointerScroll'
-import domHelpers from './domHelpers'
 
-export default { ajax, pointer, pointerScroll, domHelpers }
+export default { ajax, pointer, pointerScroll }
