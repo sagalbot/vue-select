a1cb4a9b1fb6cbaf0429d33cf456ced7a81f6f1f
Jeff <sagalbot@gmail.com>
2020-03-14 18:03:27 -0700

    add contributors component

---
+++ docs/.vuepress/components/Contributors.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul>
+    <li v-for="c in contributors">
+      <img :src="`${c.avatar_url}&s=75`" :alt="`${c.login}'s Avatar`" />
+      <div>
+        <a :href="c.html_url">@{{ c.login }}</a>
+        <br>{{ c.contributions }} contributions
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { CONTRIBUTORS } from "@dynamic/constants";
+
+export default {
+  data: () => ({
+    contributors: CONTRIBUTORS.filter(
+      ({ login }) => login !== "semantic-release-bot"
+    )
+  })
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  max-width: 100%;
+  margin-top: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+img {
+  width: 75px;
+  height: 75px;
+  margin-right: 1rem;
+  border-radius: 100%;
+}
+li {
+  display: inline-flex;
+  align-items: center;
+  margin-bottom: 2rem;
+  /*max-width: 220px;*/
+}
+  a {
+    display: inline-block;
+    max-width: 100px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap
+  }
+</style>
---
+++ docs/contributors.md
@@ -0,0 +1,9 @@
+---
+sidebarDepth: 0
+---
+
+# Contributors
+
+Vue Select is supported by a community of awesome contributors!
+
+<Contributors />
