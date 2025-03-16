06d9a373f2a5e98503ed4ff1d43a94b4f81909f2
Jeff Sagal <sagalbot@gmail.com>
2025-03-16 10:13:20 -0700

    deps: upgrade dependencies (#1867)
    * update deps
    
    * Update package.json
    
    * update deps and workflows
    
    * refactor workflows
    
    * always checkout
    
    * Update action.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * bump
    
    * Update action.yml
    
    * Update action.yml
    
    * update workflow
    
    * fix pnpm issues
    
    * upgrade eslint
    
    * complete workflows
    
    * Update eslint.config.ts
    
    * upgrade tailwind
    
    * Update .gitignore
    
    * upgrade nuxt content separately

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,28 +1,26 @@
 import { resolve } from 'path'
+import tailwindcss from '@tailwindcss/vite'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/styles/main.css'],
+
   modules: [
     '@nuxtjs/color-mode',
     '@nuxt/content',
     '@nuxtjs/google-fonts',
     'nuxt-svgo',
   ],
+
   // https://color-mode.nuxtjs.org/
   colorMode: { classSuffix: '' },
-  postcss: {
-    plugins: {
-      'tailwindcss/nesting': {},
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+
   app: {
     head: {
       title: 'Vue Select',
       meta: [{ name: 'description', content: 'My amazing site.' }],
     },
   },
+
   // https://content.nuxtjs.org/api/configuration
   content: {
     documentDriven: true,
@@ -35,15 +33,19 @@ export default defineNuxtConfig({
       },
     },
   },
+
   svgo: {
     defaultImport: 'component',
   },
+
   googleFonts: {
     families: {
       'Public+Sans': [300, 400, 500, 600],
     },
   },
+
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         // resolve the aliases used in the vue-select build
@@ -51,4 +53,6 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  compatibilityDate: '2025-03-16',
 })
---
+++ eslint.config.ts
@@ -0,0 +1,34 @@
+import pluginVue from 'eslint-plugin-vue'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript'
+import pluginVitest from '@vitest/eslint-plugin'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: [
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      'docs/**',
+      '.nuxt',
+    ],
+  },
+
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['tests/**/*'],
+  },
+  skipFormatting,
+)
